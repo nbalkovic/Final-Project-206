d99170ce38460f09a7c2c12f9884a4da39bfdc5a
Natasha <nbalkovic7@gmail.com>
2020-04-08 11:50:46 -0400

    merge

--- FinalProject.py
+++ FinalProject.py
@@ -9,7 +9,7 @@ def get_state_virus_data():
     request_url = 'https://covidtracking.com/api/v1/states/current.json'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15'}
     virus_data = requests.get(request_url, headers = headers)
-    return (vrius_data)
+    return (virus_data)
 
 def get_state_populations():
     request_url = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest'
