3c4865e4ef581707dc27da7dd71c33543c3bbe18
Justin Halper <Justin@Justins-Air.home>
2020-04-11 14:19:33 -0400

    guidelines and starting pts

--- FinalProject.py
+++ FinalProject.py
@@ -5,6 +5,8 @@ import requests
 import sqlite3
 import os
 
+#GET DATA
+
 def get_state_virus_data():
     request_url = 'https://covidtracking.com/api/v1/states/current.json'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15'}
@@ -17,6 +19,10 @@ def get_state_populations():
     population_data = requests.get(request_url, headers = headers)
     population_list = json.loads(population_data.text)
     return (population_list)
+def create_table():
+    #store 100 items from an API into a table, one table for each API
+
+#PROCESS DATA
 
 def count_state_cases():
     request_url = 'https://covidtracking.com/api/v1/states/current.json'
@@ -26,6 +32,19 @@ def count_state_cases():
     for state in virus_data:
         ordered_cases.append(state)
     return sorted(ordered_cases, key = lambda x: x[1], reverse=True)
+
+def cases_per_pop():
+    #calculates number of cases per state population (cases per capita)
+
+#VISUALIZE DATA: We just need 2 charts
+
+#REPORT
+#Goals:
+#Problems Faced: Issues with Twitter as an API
+#Include the visualizations (2 charts from earlier)
+#Instructions for running code
+#Explain what each function did, run through each briefly
+#State resources used
 
 conn = sqlite3.connect('final.sqlite')
 cur = conn.cursor()
