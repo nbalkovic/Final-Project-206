bfd1c131d2c425fcb78728d106c7dc8ee038fb07
Natasha <nbalkovic7@gmail.com>
2020-04-15 16:06:06 -0400

    merge

--- FinalProject.py
+++ FinalProject.py
@@ -20,10 +20,8 @@ def get_state_populations():
     request_url_pop = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36','Content-type': 'text/html', 'Accept-encoding':'Identity', 'upgrade-insecure-requests': '1'}
     population_data = requests.get(request_url_pop, headers = headers)
-    print('population_data',population_data.content.decode())
+    return(population_data.content.decode())
     
-    #population_list = json.loads(population_data.text)
-    #return(population_list)
 
 def get_social_data():
     url = ('https://gs.statcounter.com/social-media-stats/all/united-states-of-america/#daily-20200101-20200409')
@@ -33,7 +31,7 @@ def get_social_data():
     
     all_urls = soup
     new_urls = all_urls.find(class_ = 'raphael-group-11-hot')
-    print(new_urls)
+    return(new_urls)
 
 def get_world_info():
     url = ('https://www.worldometers.info/coronavirus/#countries')
@@ -45,7 +43,6 @@ def get_world_info():
     new_urls = all_urls.findAll(class_ = 'sorting')
     return new_urls
 
-print(get_world_info)
 
 
 #calculates number of cases per state population (cases per capita)
@@ -59,39 +56,21 @@ print(get_world_info)
 #Instructions for running code
 #Explain what each function did, run through each briefly
 #State resources used: Github obviously, twitter, instagram api websites for example code
-
 conn = sqlite3.connect('finalproject.sqlite')
 cur = conn.cursor()
-
-cur.execute('CREATE TABLE IF NOT EXISTS POPULATIONS (state TEXT, population INTEGER)')
-state_pop = get_state_populations()
-#state_pop_cache = state_pop['data']
-
-# def population_table(start_pos, end_pos):
-#     for x in range(start_pos, end_pos):
-#         if (x <= 52):
-#             row = state_pop_cache[x]
-#             state_name = row['State']
-#             state_population = row['Population']
-#             cur.execute('INSERT INTO POPULATIONS (state, population) VALUES (?, ?)',(state_name, state_population))
-#             conn.commit()
-#         else:
-#             continue
-#     start_pos = end_pos
-#     end_pos +=51
-#     return start_pos, end_pos
 
 
 def counter():
     new_list = []
+   
     cur.execute("SELECT count(*) FROM TOTAL_VIRUSES")
     total_virus = cur.fetchone()
     conn.commit()
     new_list.append(total_virus)
     
     cur.execute("SELECT count(*) FROM POPULATIONS")
-    conn.commit()
     total_pop = cur.fetchone()
+    conn.commit()
     new_list.append(total_pop)
     
     # cur.execute("SELECT count(*) FROM SOCIAL")
@@ -105,6 +84,33 @@ def counter():
     # new_list.append(total_social2)
 
     return new_list
+
+cur.execute('CREATE TABLE IF NOT EXISTS POPULATIONS (state TEXT, population INTEGER)')
+state_pop = get_state_populations()
+state_data = (state_pop['data']['state'])
+print(state_data)
+
+def population_table(state_data):
+    total_pop_record = counter()
+    counter_1 = -1
+    total_pop = total_pop_record[0][0]
+    for item in state_data:
+        counter_1+=1
+        if counter_1 < int(total_pop):
+            continue
+
+        else:
+            state_name = item['State']
+            print(state_name)
+            state_total = item['Population']
+            print(state_total)
+            cur.execute('INSERT INTO POPULATIONS (state, populations) VALUES (?, ?)',(state_name, state_total))
+            conn.commit()
+        
+            if counter_1 > 18+total_pop:
+                break
+
+
 cur.execute('CREATE TABLE IF NOT EXISTS TOTAL_VIRUSES (state TEXT, total INTEGER)')
 json_data = get_state_virus_data()
 
@@ -158,6 +164,8 @@ def main():
     counter()
     json_data = get_state_virus_data()
     total_virus_table(json_data)
+    state_data = get_state_populations()
+    population_table(state_data)
 
 if __name__ == "__main__":
     main()
